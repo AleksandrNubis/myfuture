971ef4773bcfe0ce902176e203f76a9424821cb2
oleksandr <searlashorn@gmail.com>
2020-02-08 21:49:46 +0200

    last commit

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main()  {
-	
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"myfuture/models"
+	"net/http"
+)
+
+var books []Book
+
+func main() {
+	r := mux.NewRouter()
+	books = append(books, Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+	books = append(books, Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
---
+++ models/model.go
@@ -0,0 +1,12 @@
+package models
+
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
