620bd521e64e4a38623b412714a4bc4f31b59d94
oleksandr <searlashorn@gmail.com>
2020-02-09 13:03:21 +0200

    The last* commit

--- handlers/handler.go
+++ handlers/handler.go
@@ -9,59 +9,59 @@ import (
 	"strconv"
 )
 
-var books []models.Book
+var Books []models.Book
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func GetBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(Books)
 }
 
-func getBook(w http.ResponseWriter, r *http.Request) {
+func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range books {
+	for _, item := range Books {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{})
+	json.NewEncoder(w).Encode(&models.Book{})
 }
 
-func createBook(w http.ResponseWriter, r *http.Request) {
+func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var book Book
+	var book models.Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
 	book.ID = strconv.Itoa(rand.Intn(1000000))
-	books = append(books, book)
+	Books = append(Books, book)
 	json.NewEncoder(w).Encode(book)
 }
 
-func updateBook(w http.ResponseWriter, r *http.Request) {
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range books {
+	for index, item := range Books {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
+			Books = append(Books[:index], Books[index+1:]...)
+			var book models.Book
 			_ = json.NewDecoder(r.Body).Decode(&book)
 			book.ID = params["id"]
-			books = append(books, book)
+			Books = append(Books, book)
 			json.NewEncoder(w).Encode(book)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(Books)
 }
 
-func deleteBook(w http.ResponseWriter, r *http.Request) {
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range books {
+	for index, item := range Books {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
+			Books = append(Books[:index], Books[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(Books)
 }
--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	models.books = append(books, models.Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
-	models.books = append(books, models.Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
-	r.HandleFunc("/books", handlers.getBooks).Methods("GET")
-	r.HandleFunc("/books/{id}", handlers.getBook).Methods("GET")
-	r.HandleFunc("/books", handlers.createBook).Methods("POST")
-	r.HandleFunc("/books/{id}", handlers.updateBook).Methods("PUT")
-	r.HandleFunc("/books/{id}", handlers.deleteBook).Methods("DELETE")
+	handlers.Books = append(handlers.Books, models.Book{ID: "1", Title: "Война и Мир", Author: &models.Author{Firstname: "Лев", Lastname: "Толстой"}})
+	handlers.Books = append(handlers.Books, models.Book{ID: "2", Title: "Преступление и наказание", Author: &models.Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+	r.HandleFunc("/books", handlers.GetBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", handlers.GetBook).Methods("GET")
+	r.HandleFunc("/books", handlers.CreateBook).Methods("POST")
+	r.HandleFunc("/books/{id}", handlers.UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", handlers.DeleteBook).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
